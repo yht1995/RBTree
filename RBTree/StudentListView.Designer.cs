af5d63221c5892fc69627d0272b7fbfbaa3e5f7e
yht1995 <yaoht13@mails.tsinghua.edu.cn>
2014-12-12 22:00:27 +0800

    创建ListView显示学生信息

---
+++ RBTree/StudentListView.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace RBTree
+{
+    partial class StudentListView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(317, 411);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // StudentList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 435);
+            this.Controls.Add(this.listView);
+            this.Name = "StudentList";
+            this.Text = "数据";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+    }
+}
---
+++ RBTree/StudentListView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RBTree
+{
+    public partial class StudentListView : Form
+    {
+        public StudentListView()
+        {
+            InitializeComponent();
+            InitListView(listView);
+
+        }
+
+        public void AddStudent(Student s)
+        {
+            ListViewItem item = new ListViewItem();
+            item.Text = s.Name;
+            item.SubItems.Add(s.ID);
+            item.SubItems.Add(s.Socre.ToString());
+            this.listView.Items.Add(item);
+        }
+
+        private void InitListView(ListView ListView)
+        {
+            ColumnHeader col1 = new ColumnHeader();
+            col1.Width = 100;
+            col1.Text = "姓名";
+            listView.Columns.Add(col1);
+            ColumnHeader col2 = new ColumnHeader();
+            col2.Width = 100;
+            col2.Text = "学号";
+            listView.Columns.Add(col2);
+            ColumnHeader col3 = new ColumnHeader();
+            col3.Width = 100;
+            col3.Text = "成绩";
+            listView.Columns.Add(col3);
+        }
+    }
+}
